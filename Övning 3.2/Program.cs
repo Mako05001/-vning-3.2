656e2e1c5180681d7b635c8225de5241a9d8934d
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-22 13:54:47 +0200

    Add project files.

---
+++ Övning 3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Övning3._2
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilket land vann fotboll-VM för damer 2015?");
+            string l = Console.ReadLine();
+            if (l == "usa")
+            {
+                Console.WriteLine("Rätt");
+            }
+            else if (l != "usa")
+            {
+                Console.WriteLine("Fel");
+            }
+
+        }
+    }
+}
